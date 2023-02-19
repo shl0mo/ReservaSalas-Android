4e3a4299a4afa92a131c1ff48bfe98982d548d13
shl0mo <salomao.dacostacruz@gmail.com>
2023-02-19 01:38:40 -0400

    tela cadastro usuário - 1

---
+++ app/src/main/java/com/example/reservasalas/CadastroUsuarioActivity.java
@@ -0,0 +1,14 @@
+package com.example.reservasalas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadastroUsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_usuario);
+    }
+}
--- app/src/main/java/com/example/reservasalas/MenuAdminActivity.java
+++ app/src/main/java/com/example/reservasalas/MenuAdminActivity.java
@@ -2,6 +2,7 @@ package com.example.reservasalas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MenuAdminActivity extends AppCompatActivity {
@@ -10,5 +11,10 @@ public class MenuAdminActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_admin);
+    }
+
+    public void cadastrarUsuario () {
+        Intent in = new Intent(MenuAdminActivity.this, CadastroUsuarioActivity.class);
+        startActivity(in);
     }
 }
