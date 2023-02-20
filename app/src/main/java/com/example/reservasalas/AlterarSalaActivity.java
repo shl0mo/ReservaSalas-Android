2955d78f4c519a29993609adf1eabf1c93a71260
shl0mo <salomao.dacostacruz@gmail.com>
2023-02-20 06:39:43 -0400

    alterar sala - view

---
+++ app/src/main/java/com/example/reservasalas/AlterarSalaActivity.java
@@ -0,0 +1,14 @@
+package com.example.reservasalas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlterarSalaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alterar_sala);
+    }
+}
